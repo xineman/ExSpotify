09bf852d6aeb5e9ed86de6f90d182bdb87e1a826
George <uragupalo@mail.ru>
2017-01-24 17:19:20 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,10 +1,49 @@
 $(document).ready(function() {
-  $('.header__dropdown-title').hover(function() {
-    $(this).siblings('.header__dropdown-content').toggleClass('header__dropdown-content_active');
-    $(this).find('.header__dropdown-arrow').toggleClass('header__dropdown-arrow_active');
-  });
-  $('.header__dropdown-content').hover(function() {
-    $(this).siblings('.header__dropdown-title').find('.header__dropdown-arrow').toggleClass('header__dropdown-arrow_active');
-    $(this).toggleClass('header__dropdown-content_active');
-  });
+    $('.dropdown-title').hover(
+        function() {
+            $(this).siblings('.dropdown-content').addClass('dropdown-content_active');
+            $(this).find('.dropdown-arrow').addClass('dropdown-arrow_active');
+        },
+        function() {
+            $(this).siblings('.dropdown-content').removeClass('dropdown-content_active');
+            $(this).find('.dropdown-arrow').removeClass('dropdown-arrow_active');
+        });
+    $('.dropdown-content').hover(
+        function() {
+            $(this).siblings('.dropdown-title').find('.dropdown-arrow').addClass('dropdown-arrow_active');
+            $(this).addClass('dropdown-content_active');
+        },
+        function() {
+            $(this).siblings('.dropdown-title').find('.dropdown-arrow').removeClass('dropdown-arrow_active');
+            $(this).removeClass('dropdown-content_active');
+        });
+    $('.content-header__source-dropdown').click(function(event) {
+      console.log(event.target.id);
+      $(this).find('.dropdown-content').removeClass('dropdown-content_active');
+        switch (event.target.id) {
+            case "choose-library":
+            case "choose-library-a":
+                {
+                    $('.content-header__library-source-wrapper').addClass("content-header__library-source-wrapper_active");
+                    $('.content-header__url-source-wrapper').removeClass("content-header__url-source-wrapper_active");
+                    $('#current-source').html("Source: Library");
+                    break;
+                }
+            case "choose-url":
+            case "choose-url-a":
+                {
+                    $('.content-header__library-source-wrapper').removeClass("content-header__library-source-wrapper_active");
+                    $('.content-header__url-source-wrapper').addClass("content-header__url-source-wrapper_active");
+                    $('#current-source').html("Source: Playlist URL");
+                    break;
+                }
+            default:
+
+        }
+
+    })
+    $('.content-header__library-source').click(function() {
+        $('.content-header__library-source').removeClass("content-header__library-source_active");
+        $(this).addClass("content-header__library-source_active");
+    })
 })
